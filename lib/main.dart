dc1fa61ccfbee6a8ca5c60d06d3542424221b84d
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-02-14 11:03:43 -0500

    created workout class and displayed a field on the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+//import the plan gen file to use those functions
+import 'plan_gen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -22,7 +25,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -100,6 +103,11 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            //this is text that displays
+            Text(
+              testFunc(),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
--- lib/plan_gen.dart
+++ lib/plan_gen.dart
@@ -1,1 +1,17 @@
 //first push to plan gen translate branch
+
+//class to represent a workout for the running activity
+class RunWorkout {
+  late String type; //normal run or workout
+  late String version; //time or distance
+  late double volume; //minutes, miles, meters, or seconds
+  late String intensity; //easy, hard, tempo, etc. ('none' if not applicable)
+  late double reps; //how many reps of the workout (0 if not applicable)
+  RunWorkout(this.type, this.version, this.volume, this.intensity, this.reps);
+}
+
+String testFunc() {
+  var x = "Hello there";
+  var test = new RunWorkout("run", "distance", 3.0, "easy", 0);
+  return test.type;
+}

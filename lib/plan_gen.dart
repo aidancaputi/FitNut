a0a14b9b5a5e004a44851f10fce5ffdf0edae3a7
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-02-16 21:12:00 -0500

    plan dump to test home page for viewing

--- lib/plan_gen.dart
+++ lib/plan_gen.dart
@@ -1,6 +1,8 @@
 //first push to plan gen translate branch
 
 //class to represent a workout for the running activity
+import 'dart:convert';
+
 class RunWorkout {
   String type; //normal run or workout
   String version; //time or distance
@@ -8,6 +10,14 @@ class RunWorkout {
   String intensity; //easy, hard, tempo, etc. ('none' if not applicable)
   double reps; //how many reps of the workout (0 if not applicable)
   RunWorkout(this.type, this.version, this.volume, this.intensity, this.reps);
+
+  Map toJson() => {
+        'type': type,
+        'version': version,
+        'volume': volume,
+        'intensity': intensity,
+        'reps': reps
+      };
 }
 
 //class to represent a week of a training plan (i.e. 7 workouts)
@@ -22,12 +32,45 @@ class Week {
   late RunWorkout day7;
   Week(this.day1, this.day2, this.day3, this.day4, this.day5, this.day6,
       this.day7);
+
+  Map toJson() => {
+        'day1': jsonEncode(day1),
+        'day2': jsonEncode(day2),
+        'day3': jsonEncode(day3),
+        'day4': jsonEncode(day4),
+        'day5': jsonEncode(day5),
+        'day6': jsonEncode(day6),
+        'day7': jsonEncode(day7)
+      };
+}
+
+//class to represent a user input for a running plan
+class RunPlanInput {
+  //establish all fields of the input that will be gotten from user
+  late String gender;
+  late int heightIn;
+  late int weightLbs;
+  late int age;
+  late int experienceLevel;
+  late int rhr;
+  RunPlanInput(this.gender, this.heightIn, this.weightLbs, this.age,
+      this.experienceLevel, this.rhr);
+}
+
+//function to get user input and place the input into a RunPlanInput object
+void getInput(RunPlanInput inputObject) {
+  inputObject.gender = "male";
+  inputObject.heightIn = 6 * 12; //6 foot tall
+  inputObject.weightLbs = 180; //180 lbs
+  inputObject.age = 35;
+  inputObject.experienceLevel = 5;
+  inputObject.rhr = 80;
 }
 
 //this is a dummy function for printing and testing
-String testFunc() {
-  var testDay1 = RunWorkout("run", "distance", 3.0, "easy", 0);
-  var testDay2 = RunWorkout("run", "distance", 3.0, "tempo", 0);
+List<Week> testFunc() {
+  var testDay1 = RunWorkout("workout", "distance", 3.0, "easy", 0);
+  var testDay2 = RunWorkout("workout", "distance", 3.0, "tempo", 0);
   var testDay3 = RunWorkout("run", "distance", 3.0, "easy", 0);
   var testDay4 = RunWorkout("run", "distance", 3.0, "easy", 0);
   var testDay5 = RunWorkout("run", "distance", 3.0, "easy", 0);
@@ -45,5 +88,9 @@ String testFunc() {
     Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
   ];
 
-  return plan[0].day2.intensity;
+  RunPlanInput userInput = RunPlanInput("gender", 0, 0, 0, 0, 0);
+  getInput(userInput);
+
+  return plan;
+  //return plan[0].day2.intensity;
 }

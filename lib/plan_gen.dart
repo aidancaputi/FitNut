4615bccb56cf6d5feaa6be2035b44945f3e46b0e
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-02-20 11:47:36 -0500

    increase volume and decrease volume functional

--- lib/plan_gen.dart
+++ lib/plan_gen.dart
@@ -4,13 +4,14 @@
 import 'dart:convert';
 
 class RunWorkout {
-  String type; //normal run or workout
+  String type; //run, workout, rest
   String version; //time or distance
   double volume; //minutes, miles, meters, or seconds
   String intensity; //easy, hard, tempo, etc. ('none' if not applicable)
   double reps; //how many reps of the workout (0 if not applicable)
   RunWorkout(this.type, this.version, this.volume, this.intensity, this.reps);
 
+  //this is just so that I can dump the workouts as json..will be removed
   Map toJson() => {
         'type': type,
         'version': version,
@@ -33,6 +34,7 @@ class Week {
   Week(this.day1, this.day2, this.day3, this.day4, this.day5, this.day6,
       this.day7);
 
+  //this is just so that i can dump the week as json..will be removed
   Map toJson() => {
         'day1': jsonEncode(day1),
         'day2': jsonEncode(day2),
@@ -67,30 +69,155 @@ void getInput(RunPlanInput inputObject) {
   inputObject.rhr = 80;
 }
 
+//this increases the volume of a plan by a certain percentage
+List<Week> increasePlanVolume(List<Week> plan, double percent) {
+  //create new plan to return
+  List<Week> newPlan = plan;
+
+  //for every week in the plan
+  for (var i = 0; i < plan.length; i++) {
+    newPlan[i] =
+        increaseWeekVolume(plan[i], percent); //increase the week volume
+  }
+
+  //return the new plan
+  return newPlan;
+}
+
+//this decreases the volume of a plan by a certain percentage
+List<Week> decreasePlanVolume(List<Week> plan, double percent) {
+  //create new plan to return
+  List<Week> newPlan = plan;
+
+  //for every week in the plan
+  for (var i = 0; i < plan.length; i++) {
+    newPlan[i] =
+        decreaseWeekVolume(plan[i], percent); //increase the week volume
+  }
+
+  //return the new plan
+  return newPlan;
+}
+
+Week increaseWeekVolume(Week weekToChange, double percent) {
+  //create the new week to return
+  Week newWeek = weekToChange;
+
+  //change each day of the new week
+  weekToChange.day1 = increaseDayVolume(weekToChange.day1, percent);
+  weekToChange.day2 = increaseDayVolume(weekToChange.day2, percent);
+  weekToChange.day3 = increaseDayVolume(weekToChange.day3, percent);
+  weekToChange.day4 = increaseDayVolume(weekToChange.day4, percent);
+  weekToChange.day5 = increaseDayVolume(weekToChange.day5, percent);
+  weekToChange.day6 = increaseDayVolume(weekToChange.day6, percent);
+  weekToChange.day7 = increaseDayVolume(weekToChange.day7, percent);
+
+  //return the new week
+  return newWeek;
+}
+
+//takes a week and decreases its volume by a percentage
+Week decreaseWeekVolume(Week weekToChange, double percent) {
+  //create the new week to return
+  Week newWeek = weekToChange;
+
+  //change each day of the new week
+  weekToChange.day1 = decreaseDayVolume(weekToChange.day1, percent);
+  weekToChange.day2 = decreaseDayVolume(weekToChange.day2, percent);
+  weekToChange.day3 = decreaseDayVolume(weekToChange.day3, percent);
+  weekToChange.day4 = decreaseDayVolume(weekToChange.day4, percent);
+  weekToChange.day5 = decreaseDayVolume(weekToChange.day5, percent);
+  weekToChange.day6 = decreaseDayVolume(weekToChange.day6, percent);
+  weekToChange.day7 = decreaseDayVolume(weekToChange.day7, percent);
+
+  //return the new week
+  return newWeek;
+}
+
+//takes a run workout and increases its volume by a percentage
+RunWorkout increaseDayVolume(RunWorkout dayToChange, double percent) {
+  //create the new workout to return
+  RunWorkout newWorkout = dayToChange;
+
+  //if it was a rest day, do nothing
+  if (dayToChange.type == "rest") {
+    return newWorkout;
+  } else if (dayToChange.type == "workout") {
+    //if it was a workout, increase reps
+    newWorkout.reps *= (1.0 + (percent / 100.0));
+    //newWorkout.reps = newWorkout.reps.roundToDouble();
+  } else if (dayToChange.type == "run") {
+    // if it was a time run, increase the time or distance
+    newWorkout.volume *= (1.0 + (percent / 100.0));
+    //newWorkout.volume = newWorkout.volume.roundToDouble();
+  }
+
+  //return the new workout
+  return newWorkout;
+}
+
+//takes a run workout and decreases its volume by a percentage
+RunWorkout decreaseDayVolume(RunWorkout dayToChange, double percent) {
+  //create the new workout to return
+  RunWorkout newWorkout = dayToChange;
+
+  //if it was a rest day, do nothing
+  if (dayToChange.type == "rest") {
+    return newWorkout;
+  } else if (dayToChange.type == "workout") {
+    //if it was a workout, increase reps
+    newWorkout.reps *= (1.0 - (percent / 100.0));
+    //newWorkout.reps = newWorkout.reps.roundToDouble();
+  } else if (dayToChange.type == "run") {
+    // if it was a time run, increase the time or distance
+    newWorkout.volume *= (1.0 - (percent / 100.0));
+    //newWorkout.volume = newWorkout.volume.roundToDouble();
+  }
+
+  //return the new workout
+  return newWorkout;
+}
+
 //this is a dummy function for printing and testing
 List<Week> testFunc() {
-  var testDay1 = RunWorkout("workout", "distance", 3.0, "easy", 0);
-  var testDay2 = RunWorkout("workout", "distance", 3.0, "tempo", 0);
-  var testDay3 = RunWorkout("run", "distance", 3.0, "easy", 0);
-  var testDay4 = RunWorkout("run", "distance", 3.0, "easy", 0);
-  var testDay5 = RunWorkout("run", "distance", 3.0, "easy", 0);
-  var testDay6 = RunWorkout("run", "distance", 3.0, "easy", 0);
-  var testDay7 = RunWorkout("run", "distance", 3.0, "easy", 0);
+  var testDay1 = RunWorkout("rest", "rest", 0.0, "rest", 0.0);
+  var testDay2 = RunWorkout("workout", "distance", 400, "hard", 6.0);
+  var testDay3 = RunWorkout("run", "distance", 3.0, "easy", 0.0);
+  var testDay4 = RunWorkout("run", "time", 30.0, "tempo", 0.0);
+  var testDay5 = RunWorkout("rest", "rest", 0.0, "rest", 0.0);
+  var testDay6 = RunWorkout("workout", "time", 90.0, "easy", 5.0);
+  var testDay7 = RunWorkout("run", "distance", 6.0, "easy", 0.0);
 
-  List<Week> plan = [
+  var testDay11 = RunWorkout("rest", "rest", 0.0, "rest", 0.0);
+  var testDay22 = RunWorkout("workout", "distance", 400, "hard", 6.0);
+  var testDay33 = RunWorkout("run", "distance", 3.0, "easy", 0.0);
+  var testDay44 = RunWorkout("run", "time", 30.0, "tempo", 0.0);
+  var testDay55 = RunWorkout("rest", "rest", 0.0, "rest", 0.0);
+  var testDay66 = RunWorkout("workout", "time", 90.0, "easy", 5.0);
+  var testDay77 = RunWorkout("run", "distance", 6.0, "easy", 0.0);
+
+  var testDay111 = RunWorkout("rest", "rest", 0.0, "rest", 0.0);
+  var testDay222 = RunWorkout("workout", "distance", 400, "hard", 6.0);
+  var testDay333 = RunWorkout("run", "distance", 3.0, "easy", 0.0);
+  var testDay444 = RunWorkout("run", "time", 30.0, "tempo", 0.0);
+  var testDay555 = RunWorkout("rest", "rest", 0.0, "rest", 0.0);
+  var testDay666 = RunWorkout("workout", "time", 90.0, "easy", 5.0);
+  var testDay777 = RunWorkout("run", "distance", 6.0, "easy", 0.0);
+
+  List<Week> initialPlan = [
     Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
-    Week(testDay1, testDay2, testDay3, testDay4, testDay5, testDay6, testDay7),
+    Week(testDay11, testDay22, testDay33, testDay44, testDay55, testDay66,
+        testDay77),
+    Week(testDay111, testDay222, testDay333, testDay444, testDay555, testDay666,
+        testDay777),
   ];
 
   RunPlanInput userInput = RunPlanInput("gender", 0, 0, 0, 0, 0);
   getInput(userInput);
 
-  return plan;
-  //return plan[0].day2.intensity;
+  List<Week> finalPlan = increasePlanVolume(initialPlan, 10.0);
+  finalPlan = decreasePlanVolume(finalPlan, 10.0);
+  finalPlan = increasePlanVolume(finalPlan, 20.0);
+
+  return finalPlan;
 }

d7e820945448c38eb9ba3230f2c34762f2c177e0
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-03-28 10:16:36 -0400

    half marathon innit

---
+++ lib/base_plan_files/half-marathon.dart
@@ -0,0 +1,126 @@
+import 'package:FitNut/plan_gen.dart';
+
+var raceDistance = 13.1;
+
+//week 1
+var day1 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day2 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
+var day3 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day4 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
+var day5 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day6 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 3);
+var day7 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 5);
+
+//week 2
+var day8 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day9 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
+var day10 = RunWorkout("run", "distance", 4.0, "tempo", 0.0, 2);
+var day11 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
+var day12 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day13 = RunWorkout("run", "distance", 3.0, "tempo", 0.0, 3);
+var day14 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 5);
+
+//week 3
+var day15 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day16 = RunWorkout("run", "distance", 3.5, "easy", 0.0, 2);
+var day17 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
+var day18 = RunWorkout("run", "distance", 3.5, "easy", 0.0, 2);
+var day19 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day20 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day21 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 5);
+
+//week 4
+var day22 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day23 = RunWorkout("run", "distance", 3.5, "easy", 0.0, 2);
+var day24 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 2);
+var day25 = RunWorkout("run", "distance", 3.5, "easy", 0.0, 2);
+var day26 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day27 = RunWorkout("run", "distance", 3.0, "tempo", 0.0, 3);
+var day28 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 5);
+
+//week 5
+var day29 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day30 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day31 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 2);
+var day32 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day33 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day34 = RunWorkout("run", "distance", 3.0, "tempo", 0.0, 3);
+var day35 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 5);
+
+//week 6
+var day36 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day37 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day38 = RunWorkout("run", "distance", 6.0, "tempo", 0.0, 2);
+var day39 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day40 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day41 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day42 = RunWorkout("run", "distance", 3.1, "race", 0.0, 5);
+
+//week 7
+var day43 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day44 = RunWorkout("run", "distance", 4.5, "easy", 0.0, 2);
+var day45 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 2);
+var day46 = RunWorkout("run", "distance", 4.5, "easy", 0.0, 2);
+var day47 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day48 = RunWorkout("run", "distance", 4.0, "tempo", 0.0, 3);
+var day49 = RunWorkout("run", "distance", 9.0, "easy", 0.0, 5);
+
+//week 8
+var day50 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day51 = RunWorkout("run", "distance", 4.5, "easy", 0.0, 2);
+var day52 = RunWorkout("run", "distance", 7.0, "tempo", 0.0, 2);
+var day53 = RunWorkout("run", "distance", 4.5, "easy", 0.0, 2);
+var day54 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day55 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
+var day56 = RunWorkout("run", "distance", 10.0, "easy", 0.0, 5);
+
+//week 9
+var day57 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day58 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
+var day59 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 2);
+var day60 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
+var day61 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day62 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day63 = RunWorkout("run", "distance", 6.2, "race", 0.0, 7);
+
+//week 10
+var day64 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day65 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
+var day66 = RunWorkout("run", "distance", 8.0, "tempo", 0.0, 2);
+var day67 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
+var day68 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day69 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
+var day70 = RunWorkout("run", "distance", 11.0, "easy", 0.0, 5);
+
+//week 11
+var day71 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 0);
+var day72 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
+var day73 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 2);
+var day74 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day75 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day76 = RunWorkout("run", "distance", 3.0, "tempo", 0.0, 3);
+var day77 = RunWorkout("run", "distance", 12.0, "easy", 0.0, 5);
+
+//week 12
+var day78 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day79 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
+var day80 = RunWorkout("run", "distance", 4.0, "tempo", 0.0, 2);
+var day81 = RunWorkout("run", "distance", 2.0, "easy", 0.0, 2);
+var day82 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day83 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
+var day84 = RunWorkout("run", "distance", raceDistance, "race", 0.0, 5);
+
+List<Week> baseMarathonPlan = [
+  Week(day1, day2, day3, day4, day5, day6, day7, 1), //week 1
+  Week(day8, day9, day10, day11, day12, day13, day14, 2), //week 2
+  Week(day15, day16, day17, day18, day19, day20, day21, 3), //week 3
+  Week(day22, day23, day24, day25, day26, day27, day28, 50), //week 4
+  Week(day29, day30, day31, day32, day33, day34, day35, 4), //week 5
+  Week(day36, day37, day38, day39, day40, day41, day42, 5), //week 6
+  Week(day43, day44, day45, day46, day47, day48, day49, 6), //week 7
+  Week(day50, day51, day52, day53, day54, day55, day56, 100), //week 8
+  Week(day57, day58, day59, day60, day61, day62, day63, 1), //week 9
+  Week(day64, day65, day66, day67, day68, day69, day70, 2), //week 10
+  Week(day71, day72, day73, day74, day75, day76, day77, 3), //week 11
+  Week(day78, day79, day80, day81, day82, day83, day84, 50), //week 12
+];

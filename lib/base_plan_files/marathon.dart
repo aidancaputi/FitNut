5f6cbbfc1488863a155afaf40925f1d06af90a1d
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-04-18 18:38:06 -0400

    days switched to lists of workouts

--- lib/base_plan_files/marathon.dart
+++ lib/base_plan_files/marathon.dart
@@ -3,166 +3,166 @@ import 'package:FitNut/plan_gen.dart';
 var raceDistance = 26.2;
 
 //week 1
-var day1 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day2 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day3 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day4 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day5 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day6 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
-var day7 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 5);
+var day1 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day2 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day3 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day4 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day5 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day6 = [Workout("run", "not workout", "distance", 5.0, "tempo", 0.0, 3)];
+var day7 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 5)];
 
 //week 2
-var day8 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day9 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day10 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day11 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day12 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day13 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day14 = RunWorkout("run", "distance", 9.0, "easy", 0.0, 5);
+var day8 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day9 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day10 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day11 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day12 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day13 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day14 = [Workout("run", "not workout", "distance", 9.0, "easy", 0.0, 5)];
 
 //week 3
-var day15 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day16 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day17 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day18 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day19 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day20 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
-var day21 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 5);
+var day15 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day16 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day17 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day18 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day19 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day20 = [Workout("run", "not workout", "distance", 5.0, "tempo", 0.0, 3)];
+var day21 = [Workout("run", "not workout", "distance", 6.0, "easy", 0.0, 5)];
 
 //week 4
-var day22 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day23 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day24 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 3);
-var day25 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day26 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day27 = RunWorkout("run", "distance", 6.0, "tempo", 0.0, 3);
-var day28 = RunWorkout("run", "distance", 11.0, "easy", 0.0, 5);
+var day22 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day23 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day24 = [Workout("run", "not workout", "distance", 6.0, "easy", 0.0, 3)];
+var day25 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day26 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day27 = [Workout("run", "not workout", "distance", 6.0, "tempo", 0.0, 3)];
+var day28 = [Workout("run", "not workout", "distance", 11.0, "easy", 0.0, 5)];
 
 //week 5
-var day29 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day30 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day31 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 3);
-var day32 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day33 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day34 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day35 = RunWorkout("run", "distance", 12.0, "easy", 0.0, 5);
+var day29 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day30 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day31 = [Workout("run", "not workout", "distance", 6.0, "easy", 0.0, 3)];
+var day32 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day33 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day34 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day35 = [Workout("run", "not workout", "distance", 12.0, "easy", 0.0, 5)];
 
 //week 6
-var day36 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day37 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day38 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day39 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day40 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day41 = RunWorkout("run", "distance", 6.0, "tempo", 0.0, 3);
-var day42 = RunWorkout("run", "distance", 9.0, "easy", 0.0, 5);
+var day36 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day37 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day38 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day39 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day40 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day41 = [Workout("run", "not workout", "distance", 6.0, "tempo", 0.0, 3)];
+var day42 = [Workout("run", "not workout", "distance", 9.0, "easy", 0.0, 5)];
 
 //week 7
-var day43 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day44 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day45 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 3);
-var day46 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day47 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day48 = RunWorkout("run", "distance", 7.0, "tempo", 0.0, 3);
-var day49 = RunWorkout("run", "distance", 14.0, "easy", 0.0, 5);
+var day43 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day44 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day45 = [Workout("run", "not workout", "distance", 7.0, "easy", 0.0, 3)];
+var day46 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day47 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day48 = [Workout("run", "not workout", "distance", 7.0, "tempo", 0.0, 3)];
+var day49 = [Workout("run", "not workout", "distance", 14.0, "easy", 0.0, 5)];
 
 //week 8
-var day50 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day51 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day52 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 3);
-var day53 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day54 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day55 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 3);
-var day56 = RunWorkout("run", "distance", 15.0, "easy", 0.0, 5);
+var day50 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day51 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day52 = [Workout("run", "not workout", "distance", 7.0, "easy", 0.0, 3)];
+var day53 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day54 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day55 = [Workout("run", "not workout", "distance", 7.0, "easy", 0.0, 3)];
+var day56 = [Workout("run", "not workout", "distance", 15.0, "easy", 0.0, 5)];
 
 //week 9
-var day57 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day58 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day59 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day60 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day61 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day62 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day63 = RunWorkout("run", "distance", 13.1, "race", 0.0, 5);
+var day57 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day58 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day59 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day60 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day61 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day62 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day63 = [Workout("run", "not workout", "distance", 13.1, "race", 0.0, 5)];
 
 //week 10
-var day64 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day65 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day66 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 3);
-var day67 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day68 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day69 = RunWorkout("run", "distance", 8.0, "tempo", 0.0, 3);
-var day70 = RunWorkout("run", "distance", 17.0, "easy", 0.0, 5);
+var day64 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day65 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day66 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 3)];
+var day67 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day68 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day69 = [Workout("run", "not workout", "distance", 8.0, "tempo", 0.0, 3)];
+var day70 = [Workout("run", "not workout", "distance", 17.0, "easy", 0.0, 5)];
 
 //week 11
-var day71 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day72 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day73 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 3);
-var day74 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day75 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day76 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 3);
-var day77 = RunWorkout("run", "distance", 18.0, "easy", 0.0, 5);
+var day71 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day72 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day73 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 3)];
+var day74 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day75 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day76 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 3)];
+var day77 = [Workout("run", "not workout", "distance", 18.0, "easy", 0.0, 5)];
 
 //week 12
-var day78 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day79 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day80 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day81 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day82 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day83 = RunWorkout("run", "distance", 8.0, "tempo", 0.0, 3);
-var day84 = RunWorkout("run", "distance", 13.0, "easy", 0.0, 5);
+var day78 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day79 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day80 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day81 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day82 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day83 = [Workout("run", "not workout", "distance", 8.0, "tempo", 0.0, 3)];
+var day84 = [Workout("run", "not workout", "distance", 13.0, "easy", 0.0, 5)];
 
 //week 13
-var day85 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day86 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day87 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 3);
-var day88 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day89 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day90 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
-var day91 = RunWorkout("run", "distance", 20.0, "easy", 0.0, 5);
+var day85 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day86 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day87 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 3)];
+var day88 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day89 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day90 = [Workout("run", "not workout", "distance", 5.0, "tempo", 0.0, 3)];
+var day91 = [Workout("run", "not workout", "distance", 20.0, "easy", 0.0, 5)];
 
 //week 14
-var day92 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day93 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day94 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day95 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day96 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day97 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 3);
-var day98 = RunWorkout("run", "distance", 12.0, "easy", 0.0, 5);
+var day92 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day93 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day94 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day95 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day96 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day97 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 3)];
+var day98 = [Workout("run", "not workout", "distance", 12.0, "easy", 0.0, 5)];
 
 //week 15
-var day99 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day100 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day101 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 3);
-var day102 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day103 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day104 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
-var day105 = RunWorkout("run", "distance", 20.0, "easy", 0.0, 5);
+var day99 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day100 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day101 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 3)];
+var day102 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day103 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day104 = [Workout("run", "not workout", "distance", 5.0, "tempo", 0.0, 3)];
+var day105 = [Workout("run", "not workout", "distance", 20.0, "easy", 0.0, 5)];
 
 //week 16
-var day106 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day107 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day108 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 3);
-var day109 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 2);
-var day110 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day111 = RunWorkout("run", "distance", 4.0, "tempo", 0.0, 3);
-var day112 = RunWorkout("run", "distance", 12.0, "easy", 0.0, 5);
+var day106 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day107 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day108 = [Workout("run", "not workout", "distance", 6.0, "easy", 0.0, 3)];
+var day109 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 2)];
+var day110 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day111 = [Workout("run", "not workout", "distance", 4.0, "tempo", 0.0, 3)];
+var day112 = [Workout("run", "not workout", "distance", 12.0, "easy", 0.0, 5)];
 
 //week 17
-var day113 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day114 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day115 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day116 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 2);
-var day117 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day118 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 3);
-var day119 = RunWorkout("run", "distance", 8.0, "easy", 0.0, 5);
+var day113 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day114 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day115 = [Workout("run", "not workout", "distance", 5.0, "easy", 0.0, 3)];
+var day116 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 2)];
+var day117 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day118 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 3)];
+var day119 = [Workout("run", "not workout", "distance", 8.0, "easy", 0.0, 5)];
 
 //week 18
-var day120 = RunWorkout("cross", "cross", 0.0, "cross", 0.0, 1);
-var day121 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day122 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 3);
-var day123 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day124 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day125 = RunWorkout("run", "distance", 2.0, "easy", 0.0, 3);
-var day126 = RunWorkout("race", "race", raceDistance, "race", 0.0, 5);
+var day120 = [Workout("cross", "cross", "cross", 0.0, "cross", 0.0, 1)];
+var day121 = [Workout("run", "not workout", "distance", 3.0, "easy", 0.0, 2)];
+var day122 = [Workout("run", "not workout", "distance", 4.0, "easy", 0.0, 3)];
+var day123 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day124 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day125 = [Workout("run", "not workout", "distance", 2.0, "easy", 0.0, 3)];
+var day126 = [Workout("race", "race", "race", raceDistance, "race", 0.0, 5)];
 
 List<Week> baseMarathonPlan = [
   Week(day1, day2, day3, day4, day5, day6, day7, 1), //week 1
--- lib/base_plan_files/sprint-tri.dart
+++ lib/base_plan_files/sprint-tri.dart
@@ -2,69 +2,78 @@ import 'package:FitNut/plan_gen.dart';
 
 var raceDistance = 3.1;
 
-var day1 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day2 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day3 = RunWorkout("workout", "distance", 400, "hard", 5.0, 3);
-var day4 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day5 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day6 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day7 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 5);
+//notes:
 
-var day8 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day9 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day10 = RunWorkout("run", "time", 30.0, "tempo", 0.0, 4);
-var day11 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day12 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day13 = RunWorkout("run", "distance", 3.0, "tempo", 0.0, 3);
-var day14 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 5);
+//swim [workouts are in meters for volume
+//bike [workouts are in seconds for volume
 
-var day15 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day16 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day17 = RunWorkout("workout", "distance", 400, "hard", 6.0, 4);
-var day18 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day19 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day20 = RunWorkout("run", "distance", 4.0, "easy", 0.0, 3);
-var day21 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 5);
+//week 1
+var day1 = [Workout("run", "not [workout", "time", 16.0, "easy", 0.0, 0)];
+var day2 = [Workout("bike", "not [workout", "time", 20.0, "easy", 0.0, 0)];
+var day3 = [Workout("swim", "[workout", "distance", 25.0, "hard", 10.0, 0)];
+var day4 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day5 = [Workout("run", "not [workout", "time", 24.0, "easy", 0.0, 0)];
+var day6 = [Workout("swim", "[workout", "distance", 50.0, "hard", 10.0, 0)];
+var day7 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
 
-var day22 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day23 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day24 = RunWorkout("run", "time", 35.0, "tempo", 0.0, 4);
-var day25 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 0);
-var day26 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day27 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day28 = RunWorkout("run", "distance", raceDistance, "race", 0.0, 7);
+//week 2
+var day8 = [Workout("run", "not [workout", "time", 20.0, "easy", 0.0, 0)];
+var day9 = [Workout("bike", "not [workout", "time", 30.0, "easy", 0.0, 0)];
+var day10 = [Workout("swim", "[workout", "distance", 100.0, "hard", 5.0, 0)];
+var day11 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day12 = [Workout("run", "not [workout", "distance", 2.0, "easy", 0.0, 0)];
+var day13 = [Workout("swim", "not [workout", "time", 10.0, "easy", 0.0, 0)];
+var day14 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
 
-var day29 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day30 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day31 = RunWorkout("workout", "distance", 400, "hard", 7.0, 4);
-var day32 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day33 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day34 = RunWorkout("run", "distance", 4.0, "tempo", 0.0, 3);
-var day35 = RunWorkout("run", "distance", 6.0, "easy", 0.0, 5);
+//week 3
+var day15 = [Workout("run", "not [workout", "distance", 1.0, "easy", 0.0, 0)];
+var day16 = [Workout("bike", "not [workout", "time", 45.0, "easy", 0.0, 0)]; //
+var day17 = [Workout("swim", "[workout", "distance", 50.0, "hard", 10.0, 0)]; //10 x 50m
+var day18 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)]; //
+var day19 = [Workout("run", "not [workout", "distance", 1.5, "easy", 0.0, 0)]; //
+var day20 = [Workout("bike", "[workout", "time", 120.0, "hard", 10.0, 0)]; //10 x 120 sec
+var day21 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
 
-var day36 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day37 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day38 = RunWorkout("run", "time", 40.0, "tempo", 0.0, 4);
-var day39 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day40 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day41 = RunWorkout("run", "distance", 5.0, "easy", 0.0, 3);
-var day42 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 5);
+//week 4
+var day22 = [Workout("run", "not [workout", "distance", 2.0, "easy", 0.0, 0)];
+var day23 = [Workout("bike", "not [workout", "time", 50.0, "easy", 0.0, 0)];
+var day24 = [Workout("swim", "not [workout", "time", 15.0, "easy", 0.0, 0)]; //
+var day25 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day26 = [Workout("run", "[workout", "time", 60.0, "hard", 10.0, 0)]; //10 x 60 sec
+var day27 = [Workout("bike", "[workout", "time", 120.0, "hard", 10.0, 0)];
+var day28 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
 
-var day43 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day44 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day45 = RunWorkout("workout", "distance", 400, "hard", 8.0, 4);
-var day46 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day47 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day48 = RunWorkout("run", "distance", 5.0, "tempo", 0.0, 3);
-var day49 = RunWorkout("run", "distance", 7.0, "easy", 0.0, 5);
+var day29 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day30 = [Workout("run", "distance", 3.0, "easy", 0.0, 2)];
+var day31 = [Workout("workout", "distance", 400, "hard", 7.0, 4)];
+var day32 = [Workout("run", "distance", 3.0, "easy", 0.0, 2)];
+var day33 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day34 = [Workout("run", "distance", 4.0, "tempo", 0.0, 3)];
+var day35 = [Workout("run", "distance", 6.0, "easy", 0.0, 5)];
 
-var day50 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day51 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day52 = RunWorkout("run", "time", 30.0, "tempo", 0.0, 4);
-var day53 = RunWorkout("run", "distance", 3.0, "easy", 0.0, 2);
-var day54 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day55 = RunWorkout("rest", "rest", 0.0, "rest", 0.0, 0);
-var day56 = RunWorkout("race", "race", raceDistance, "race", 0.0, 5);
+var day36 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day37 = [Workout("run", "distance", 3.0, "easy", 0.0, 2)];
+var day38 = [Workout("run", "time", 40.0, "tempo", 0.0, 4)];
+var day39 = [Workout("run", "distance", 3.0, "easy", 0.0, 2)];
+var day40 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day41 = [Workout("run", "distance", 5.0, "easy", 0.0, 3)];
+var day42 = [Workout("run", "distance", 7.0, "easy", 0.0, 5)];
+
+var day43 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day44 = [Workout("run", "run", "distance", 3.0, "easy", 0.0, 2)];
+var day45 = [Workout("run", "[workout", "distance", 400, "hard", 8.0, 4)];
+var day46 = [Workout("run", "run", "distance", 3.0, "easy", 0.0, 2)];
+var day47 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day48 = [Workout("run", "run", "distance", 5.0, "tempo", 0.0, 3)];
+var day49 = [Workout("run", "run", "distance", 7.0, "easy", 0.0, 5)];
+
+var day50 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day51 = [Workout("run", "run", "distance", 3.0, "easy", 0.0, 2)];
+var day52 = [Workout("run", "run", "time", 30.0, "tempo", 0.0, 4)];
+var day53 = [Workout("run", "run", "distance", 3.0, "easy", 0.0, 2)];
+var day54 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day55 = [Workout("rest", "rest", "rest", 0.0, "rest", 0.0, 0)];
+var day56 = [Workout("race", "race", "race", raceDistance, "race", 0.0, 5)];
 
 List<Week> base5kPlan = [
   Week(day1, day2, day3, day4, day5, day6, day7, 1),
